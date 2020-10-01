b5dcc2a0a21b6796b17fd27ab9475a058749db7b
cXzo <71940015+cXzo@users.noreply.github.com>
2020-10-01 17:54:20 +0700

    Add files via upload
    Heker

---
+++ Hek.sh
@@ -0,0 +1,176 @@
+#!/usr/bin/bash
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo "                        X"
+echo  $blue "<|======================================|>"
+echo  $red    "|       TOOLS HACK SATELIT          |"
+echo  $red    "|                                   |"
+echo  $red    "| Author        : ???????           |"
+echo  $red    "| Contact       : 08xxxxxxxxxxx     |"
+echo  $red    "| My Team       : ????????????????? |"
+echo  $red    "| Thanks To     : ??????????        |"
+echo  $blue "<|=====================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Geser Satelit Palapa       $blue ||"
+echo $blue "|| $green 2. Geser Satelit Telkom       $blue ||"
+echo $blue "|| $green 3. Geser Satelit Asiasat      $blue ||"
+echo $blue "|| $green 4. Geser Satelit Nasa         $blue ||"
+echo $blue "|| $green 0. Exit The Program           $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@Heker=•>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@heker=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@Heker=•>" derajat;
+sleep 1
+echo $red "Lomonyet"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasi  Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@heker=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@heker=•>" derajat;
+sleep 1
+echo $red "Loading nyet"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@heker=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@heker=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@hekerBerkelas=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@heker=•>" derajat;
+sleep 1
+echo $red "heker "
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "Bay Bay :v"
+echo  $blue "Jangan lupa mandi hari ini "
+echo  $green "hallo bang jago"
+exit
+fi
